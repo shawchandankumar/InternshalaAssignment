4133e40c5efddc13b1629bb6dd11b9ce0176c1cd
shawchandankumar20 <shawchandankumar20@gmail.com>
2022-10-10 23:21:07 +0530

    configured mongodb Atlas

--- src/main/java/com/healthcare/Product/ProductApplication.java
+++ src/main/java/com/healthcare/Product/ProductApplication.java
@@ -2,10 +2,8 @@ package com.healthcare.Product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class ProductApplication {
 
 	public static void main(String[] args) {
